c3f5cbfb379815fa4490c31fb383b9accfa6c1db
2289589441 <2289589441@qq.com>
2018-10-13 10:47:08 +0800

    CRM最小环境

---
+++ src/main/java/com/shsxt/crm/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.shsxt.crm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.HttpServletRequest;
+/**
+ * @author 康晓伟
+ * @date: 2018/10/12 16:25
+ * @description: shsxt_crm
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping("main")
+    public String index(HttpServletRequest request){
+        request.setAttribute("ctx",request.getContextPath());
+        return "main";
+    }
+}
---
+++ src/main/java/com/shsxt/crm/interceptors/LoginInterceptor.java
@@ -0,0 +1,10 @@
+package com.shsxt.crm.interceptors;
+
+/**
+ * @author 康晓伟
+ * @auther: 康晓伟
+ * @date: 2018/10/12 16:35
+ * @description: shsxt_crm
+ */
+public class LoginInterceptor {
+}
